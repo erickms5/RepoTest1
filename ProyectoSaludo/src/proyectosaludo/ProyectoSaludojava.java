d3e2989b76e28406572b23dad8c9c86e1b6ce15e
erickms5 <erick.emelec.m@gmail.com>
2024-10-17 19:32:30 -0500

    Subiendo mi primer saludo :D

---
+++ ProyectoSaludo/src/proyectosaludo/ProyectoSaludojava.java
@@ -0,0 +1,7 @@
+package proyectosaludo;
+public class ProyectoSaludojava {
+    public static void main(String[] args) {
+     System.out.println("Hola desde NB hacia GitHub de noche D");
+    }
+    
+}
